9e896077df346a78308732f1771a299865c95def
Sampath Eaty <sam@msu.edu>
2024-01-27 20:38:02 -0500

    Change setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-npx --yes create-next-app@latest next-app --use-npm --typescript --eslint --app --src-dir --no-tailwind --no-import-alias
+npx --yes create-next-app@latest next-app --use-npm --typescript --eslint --app --src-dir --no-tailwind --import-alias "@/*"
