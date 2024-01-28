4fce7c130232914c2b5eaa61a5180df78ee7cd22
Sampath Eaty <sam@msu.edu>
2024-01-27 21:30:46 -0500

    clean up all traces

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 npx --yes create-next-app@latest next-app --use-npm --typescript --eslint --app --src-dir --no-tailwind --import-alias "@/*"
 
+rm -rf .devcontainer
+
 # Add all changes to git
 git add .
 
@@ -10,3 +12,5 @@ git commit -m "Initial setup"
 
 # Push the changes to the main branch
 git push origin main
+
+rm --'$0'
