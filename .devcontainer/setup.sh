db471972d550452d137794faee06461761bfd2d3
Sampath Eaty <sam@msu.edu>
2024-01-27 21:28:57 -0500

    delete next-app

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+
 npx --yes create-next-app@latest next-app --use-npm --typescript --eslint --app --src-dir --no-tailwind --import-alias "@/*"
+
+# Add all changes to git
+git add .
+
+# Commit the changes
+git commit -m "Initial setup"
+
+# Push the changes to the main branch
+git push origin main
